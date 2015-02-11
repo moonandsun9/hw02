38822d57f676ccf1ac7ce92f449ca10cc89a44fd
JuanTest <upci.pe1501@gmail.com>
2015-02-11 01:43:12 -0600

    avances 1
    mis primeros avances de la aplicacion

--- hw_01_id_27.cpp
+++ hw_01_id_27.cpp
@@ -1,19 +1,26 @@
-// Author:
-// Date:
-// Version:
+// Author:Juan Pueblo
+// Date:  today
+// Version:2.0
 
 /*
-Homework DD
-this assignment  ...
+Homework 2.0
+this assignment size in bytes for some data types
 */
 
 #include <iostream>
 
 int main ()
 {
-  // TODO your code here!!!!
+
+  cout << "Unsigned short: " << sizeof(unsigned short) << endl;
+  cout << "Unsigned int: " << sizeof(unsigned int) << endl;
+  cout << "Unsigned long: " << sizeof(unsigned long) << endl;
 
   std::cout<<"Hello world!!" <<std::endl;
 
   return 0;
 }
+
+
+
+
