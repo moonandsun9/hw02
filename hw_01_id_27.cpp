f6809a3e3bc329d2e02e2b8483beede2e560de87
Haruso <upci.pe1501@gmail.com>
2015-02-19 22:38:55 -0600

    Implementación  de tarea
    Solución a la tarea, versión compilable validada sin errores y
    cumpliendo las especificaciones señaladas. Version 1.1

--- hw_01_id_27.cpp
+++ hw_01_id_27.cpp
@@ -1,24 +1,27 @@
-// Author:Juan Pueblo
+// Author:Violeta Haruso
 // Date:  today
 // Version:2.0
 
 /*
 Homework 2.0
 this assignment size in bytes for some data types
+
+	
 */
 
 #include <iostream>
 
-int main ()
+int main(int argc, char **argv)
 {
-
-  cout << "Unsigned short: " << sizeof(unsigned short) << endl;
-  cout << "Unsigned int: " << sizeof(unsigned int) << endl;
-  cout << "Unsigned long: " << sizeof(unsigned long) << endl;
-
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+	std::cout<<"El tamanio de un entero es:\t\t"       << sizeof(int)    << "bytes. \n";
+	std::cout<<"El tamanio de un entero corto es:\t"   << sizeof(short)  << "bytes. \n";
+	std::cout<<"El tamanio de un entero largo es:\t"   << sizeof(long)   << "bytes. \n";
+	std::cout<<"El tamanio de un caracter es:\t\t"     << sizeof(char)   << "bytes. \n";
+	std::cout<<"El tamanio de un punto flotante es:\t" << sizeof(float)  << "bytes. \n";
+	std::cout<<"El tamanio de un doble es:\t\t"        << sizeof(double) << "bytes. \n";
+	std::cout<<"El tamanio de un booleano es:\t\t"     << sizeof(bool)   << "bytes. \n";
+	
+	return 0;
 }
 
 
